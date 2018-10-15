40f6ea68966ebd5c67fefc759be8d94b13ea9021
fseaton <fseaton@hotmail.co.uk>
2018-10-15 12:46:55 +0100

    Removed code for reading in data, as now in separate file

--- Multifractals v4.R
+++ Multifractals v4.R
@@ -27,7 +27,6 @@ library(Matrix)
 library(gridExtra)
 
 ## get data ####
-GMEP <- read.csv(***REMOVED***)
 summary(GMEP)
 apply(GMEP, 2, anyNA)
 GMEP[is.na(GMEP)] <- 0
@@ -323,28 +322,6 @@ TT.plot(tri.data = Res_fil_wide_cor_dt, main = "Texture classes",
         class.lab.col = "gray60", class.line.col	= "gray50", cex=0.7)
 
 ### Comparison to other data ####
-library(RODBC)
-
-channel1 <- odbcConnect(***REMOVED***)
-
-## list tables under GMEP_DERIVED
-sqlTables(channel1, schema="GMEP_DERIVED")
-
-Soil <- sqlFetch(channel1, "GMEP_DERIVED.WP8_SOILMETRICS", na.strings = "-9999.999")
-summary(Soil)
-
-Ancil <- sqlFetch(channel1, "GMEP_DERIVED.WP8_SOIL_ANCILLARY_Y1_4", na.strings = "-9999.999")
-summary(Ancil)
-Ancil <- Ancil[!is.na(Ancil$REP_ID),]
-
-## Microbial diversity not in sql database yet so fetching from server
-MIC_RICH <- read.csv(***REMOVED***)
-FUNG_RICH_BLAST <- read.csv(***REMOVED***, row.names = 1)
-colnames(FUNG_RICH_BLAST) <- c("REP_ID", paste0(colnames(FUNG_RICH_BLAST)[2:12], "_BLAST"))
-
-MIC_RICH <- merge(MIC_RICH, FUNG_RICH_BLAST, by="REP_ID", all=T)
-summary(MIC_RICH)
-
 
 ## filtering results by habitat ####
 BH <- select(Ancil, REP_ID, BH = BROAD_HABITAT_SURVEYOR)
